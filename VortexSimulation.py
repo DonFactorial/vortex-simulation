03a142450aa9dbf6a1ecec97f5217a9450428d9f
DonFactorial <donfactorial@gmail.com>
2025-12-18 16:07:20 -0800

    Updated real and imaginary parts

--- VortexSimulation.py
+++ VortexSimulation.py
@@ -4,14 +4,15 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 def rhsfunc_fft(t, omegafvec, nu, KX, KY, K2, n):
-    omegaf = omegafvec.reshape((n,n))
+    omegaf = (omegafvec[:n**2]+1j*omegafvec[n**2:]).reshape((n,n))
     psif = -omegaf/K2
     psi_x = np.real(np.fft.ifft2(1j*KX*psif))
     psi_y = np.real(np.fft.ifft2(1j*KY*psif))
     omega_x = np.real(np.fft.ifft2(1j*KX*omegaf))
     omega_y = np.real(np.fft.ifft2(1j*KY*omegaf))
     omegat = -np.fft.fft2(psi_x*omega_y)+np.fft.fft2(psi_y*omega_x)-nu*K2*omegaf
-    return omegat.reshape(n**2)
+    omegatvec = omegat.reshape(n**2)
+    return np.concatenate((np.real(omegatvec), np.imag(omegatvec)))
 
 # Simulation parameters
 n = 2**7
@@ -36,6 +37,7 @@ tspan = np.arange(0, 180+0.5, 0.5)
 omega0 = np.exp(-(xvals+3)**2/5 - ((yvals+3)**2)/5) + np.exp(-(xvals-3)**2/5 - ((yvals+3)**2)/5) + np.exp(-(xvals+3)**2/5 - ((yvals-3)**2)/5) + np.exp(-(xvals-3)**2/5 - ((yvals-3)**2)/5)
 omega0f = np.fft.fft2(omega0)
 omega0fvec = omega0f.reshape(N)
+omega0fvec = np.concatenate((np.real(omega0fvec), np.imag(omega0fvec)))
 
 # Time step through equation
 sol = solve_ivp(rhsfunc_fft, t_span=(tspan[0], tspan[-1]), y0=omega0fvec, t_eval=tspan, args=(nu, K_x, K_y, K, n))
@@ -45,6 +47,7 @@ fig, ax = plt.subplots()
 ims = []
 for i in range(tspan.size):
     o = sol.y[:,i]
+    o = o[:N]+1j*o[N:]
     if i==0:
         im = ax.imshow(np.real(np.fft.ifft2(o.reshape((n,n)))))
     else:
@@ -61,6 +64,7 @@ fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 ims = []
 for i in range(tspan.size):
     o = sol.y[:,i]
+    o = o[:N]+1j*o[N:]
     if i==0:
         im = ax.plot_wireframe(xvals, yvals, np.real(np.fft.ifft2(o.reshape((n,n)))))
     else:
